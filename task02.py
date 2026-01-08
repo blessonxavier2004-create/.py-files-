74a09845b922c7ef6b30ba95c30ae84239a35a8b
blesson xavier <blessonxavier2004@gmail.com>
2026-01-08 17:42:47 +0530

    Update task02.py

--- task02.py
+++ task02.py
@@ -1,4 +1,4 @@
 a=input("enter the first name:")
 b=input("enter the last name:")
-c=a+b
-print("hello 'c' welcome to python programing")
+c=a+" "+b
+print(f"\nHello, {c},! Welcome to the Python course module 2 assignment.")
